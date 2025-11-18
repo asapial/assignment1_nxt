18412eecdcd9762db9deacbf546a14a36f916993
asapial <abusyeed2001@gmail.com>
2025-11-18 19:21:44 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,17 @@
+function formatValue (value : number | string | boolean){
+
+    if(typeof value === 'number'){
+        return value*10;
+    }
+    else if( typeof value === 'string'){
+        return value.toUpperCase();
+    }
+    else if(typeof value === 'boolean'){
+        return !value;
+    }
+}
+
+// console.log(formatValue('hello'));
+// console.log(formatValue(5));
+// console.log(formatValue(true));
+
