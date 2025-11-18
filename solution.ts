72e01c74c0698c5eef0dedf88173a6b37c9393f0
asapial <abusyeed2001@gmail.com>
2025-11-18 19:30:20 +0600

    Refactor getLength function and add Person class with getDetails method

--- solution.ts
+++ solution.ts
@@ -15,3 +15,36 @@ function formatValue (value : number | string | boolean){
 // console.log(formatValue(5));
 // console.log(formatValue(true));
 
+
+function getLength (value: string |any[]){
+
+    if(typeof value === 'string'){
+        return value.length;
+    }
+    else if( Array.isArray(value)){
+        return value.length;
+    }
+} 
+
+// console.log(getLength('typescript'));
+// console.log(getLength([10, 20, 30, 40]));
+
+class Person {
+    name:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+
+    getDetails (){
+        return `'Name: ${this.name}, Age: ${this.age}'`;
+    } 
+}
+
+// const person1 = new Person('John Doe', 30);
+// console.log(person1.getDetails());
+
+// const person2 = new Person('Alice', 25);
+// console.log(person2.getDetails());
