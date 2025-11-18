7e8b113272f20755e9c05334366d0b3daada0e5f
asapial <abusyeed2001@gmail.com>
2025-11-18 19:48:27 +0600

    Add filter functions for items and active users, and implement book details printer

--- solution.ts
+++ solution.ts
@@ -48,3 +48,66 @@ class Person {
 
 // const person2 = new Person('Alice', 25);
 // console.log(person2.getDetails());
+
+
+type Items={
+    title:string;
+    rating:number;
+}
+
+
+function filterByRating (value:Items[]):Items[]{
+    const sortedItems = value.filter(item => item.rating > 4);
+    return sortedItems;
+} 
+
+// const books = [
+//   { title: 'Book A', rating: 4.5 },
+//   { title: 'Book B', rating: 3.2 },
+//   { title: 'Book C', rating: 5.0 },
+// ];
+
+// console.log(filterByRating(books));
+
+type User={
+    id:number;
+    name:string;
+    email:string;
+    isActive:boolean;
+}
+
+
+function filterActiveUsers (value:User[]):User[]{
+    const activeUser=value.filter(user=>user.isActive);
+    return activeUser;
+}
+
+// const users = [
+//   { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+//   { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+//   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+// ];
+
+// console.log(filterActiveUsers(users));
+
+
+interface Book{
+    title:string;
+    author:string;
+    publishedYear:number;
+    isAvailable:boolean;
+}
+
+function printBookDetails(book:Book){
+    
+    console.log(  `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable ? "Yes" : "No"}`);
+} 
+
+// const myBook: Book = {
+//   title: 'The Great Gatsby',
+//   author: 'F. Scott Fitzgerald',
+//   publishedYear: 1925,
+//   isAvailable: false,
+// };
+
+// printBookDetails(myBook);
